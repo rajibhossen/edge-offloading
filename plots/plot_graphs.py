268fe63a627a3d330a7996be266d3c92117f64ce
Rajib Hossen <rajibcse2k10@gmail.com>
2020-04-17 02:17:02 -0500

    collected for 0.001

---
+++ plots/plot_graphs.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter1d
+
+df = pd.read_csv('../data/loss-lr-0.001-b1024-rm-40k.csv')
+# df = pd.read_csv('../data/reward_avg-lr-0.001-b1024-rm-40k.csv')
+
+# fig = px.line(df, x = 'Step', y = 'Value', title='Loss function')
+x = df['Step']
+y = df['Value']
+ysm = gaussian_filter1d(y, sigma=1.3)
+
+plt.plot(x, ysm)
+plt.xlabel("Episodes")
+plt.ylabel("Value")
+plt.title("Loss function")
+# plt.ylabel("Average Reward")
+# plt.title("Average Reward Over Episodes")
+plt.show()
