d37d42d1c83d9b6e45bae59ca9203265e80f58cd
Rajib Hossen <rajibcse2k10@gmail.com>
2020-04-21 13:32:28 -0500

    collected data

--- plots/plot_graphs.py
+++ plots/plot_graphs.py
@@ -1,20 +1,49 @@
+import re
+
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d
+import glob
 
-# df = pd.read_csv('../data/loss-lr-0.01-b1024-rm-40k.csv')
-df = pd.read_csv('../data/reward_avg-lr-0.01-b1024-rm-40k.csv')
 
-# fig = px.line(df, x = 'Step', y = 'Value', title='Loss function')
-x = df['Step']
-y = df['Value']
-ysm = gaussian_filter1d(y, sigma=1.3)
+def draw_picture(filename):
+    # df = pd.read_csv('../data/loss-lr-0.01-b1024-rm-40k.csv')
+    df = pd.read_csv(filename)
 
-plt.plot(x, ysm)
+    # fig = px.line(df, x = 'Step', y = 'Value', title='Loss function')
+    x = df['Step']
+    y = df['Value']
+    ysm = gaussian_filter1d(y, sigma=1.3)
+
+    plt.plot(x, ysm)
+    plt.xlabel("Episodes")
+    plt.ylabel("Value")
+    plt.title("Loss function")
+    # plt.ylabel("Average Reward")
+    # plt.title("Average Reward Over Episodes")
+    plt.show()
+
+
+fig = plt.figure()
+legends = []
+files = glob.glob("../data/loss-*-rm-40k.csv")
+for file in files:
+    # draw_picture(file)
+    df = pd.read_csv(file)
+    x = df['Step']
+    y = df['Value']
+    ysm = gaussian_filter1d(y, sigma=1.3)
+    lr = re.search('lr-(.+?)-b', file)
+    rm = re.search('rm-(.+?)k', file)
+    lr = lr.group(1)
+    rm = rm.group(1)
+    legends.append(str(lr) + "/" + str(rm))
+    # plt.legend("lr-" + str(m.group(1)))
+    plt.plot(x, ysm)
+
+plt.legend(legends)
 plt.xlabel("Episodes")
 plt.ylabel("Value")
 plt.title("Loss function")
-# plt.ylabel("Average Reward")
-# plt.title("Average Reward Over Episodes")
 plt.show()
